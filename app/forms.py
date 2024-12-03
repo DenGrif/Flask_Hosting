7b7c1e8f9b2c0bd2ff7932667291a3edab6d7010
DenGrif <grifon991@gmail.com>
2024-12-03 15:04:35 +0400

    Finish_polnostyu_rabotaet

--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
-
 from app.models import User
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    username = StringField('Имя пользователя', validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    confirm_password = PasswordField('Повтор пароля', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Регистрация')
 
     def validate_username(self, username):
@@ -16,6 +15,6 @@ class RegistrationForm(FlaskForm):
             raise ValidationError('Такое имя уже существует.')
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
     submit = SubmitField('Вход')
